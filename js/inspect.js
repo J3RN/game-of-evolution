4694e27419aff8c05337128af35aa8b85d4e928c
Jonathan Arnett <jonarnett90@gmail.com>
2015-10-22 20:59:29 -0400

    Inspect fix and clearing of table

--- js/inspect.js
+++ js/inspect.js
@@ -8,8 +8,8 @@ function inspectCreature(mouseEvent) {
     var xPercent = mouseEvent.offsetX / element.offsetWidth;
     var yPercent = mouseEvent.offsetY / element.offsetWidth;
 
-    var boardX = Math.floor(xPercent * boardSide) - 1;
-    var boardY = Math.floor(yPercent * boardSide) - 1;
+    var boardX = Math.floor(xPercent * boardSide);
+    var boardY = Math.floor(yPercent * boardSide);
 
     var item = GAME.getItem(boardX, boardY);
 
@@ -18,6 +18,10 @@ function inspectCreature(mouseEvent) {
 
         Object.keys(description).forEach(function(item) {
             document.getElementById(item).textContent = description[item];
+        });
+    } else {
+        Object.keys(DNA.describeDNA(GAME.creatures[0].dna)).forEach(function(item) {
+            document.getElementById(item).textContent = "";
         });
     }
 }
